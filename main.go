5f65c8b50a997c5646853ac70d8dd218f8c394a4
Cedric Darne <cedric.darne@shopify.com>
2023-12-15 10:42:37 -0500

    Refactoring of the http server
    - broken down in functions
    - signal handling for shutdown
    - proper graceful shutdown implementation

--- main.go
+++ main.go
@@ -1,21 +1,38 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"golang.org/x/net/webdav"
 )
 
 func main() {
-	port := flag.String("p", "8100", "port to serve on")
+	port := flag.Uint("p", 8100, "port to serve on")
 	directory := flag.String("d", ".", "the directory for WebDAV to serve")
 	flag.Parse()
 
-	// fileHandler := http.FileServer(http.Dir(*directory))
+	server := setupServer(*port, *directory)
+	go serve(server)
+	waitForSignal()
+	shutdown(server)
+	log.Println("server exited gracefully")
+}
+
+func setupServer(port uint, rootDirectory string) *http.Server {
+	server := &http.Server{Addr: fmt.Sprintf(":%d", port)}
+
+	// fileHandler := http.FileServer(http.Dir(rootDirectory))
 	webDAVHandler := &webdav.Handler{
-		FileSystem: webdav.Dir(*directory),
+		FileSystem: webdav.Dir(rootDirectory),
 		LockSystem: webdav.NewMemLS(),
 		Logger: func(r *http.Request, err error) {
 			if err != nil {
@@ -50,6 +67,31 @@ func main() {
 		// }
 	}))
 
-	log.Printf("Serving %s on HTTP port: %s\n", *directory, *port)
-	log.Fatal(http.ListenAndServe(":"+*port, nil))
+	log.Printf("Serving %s on HTTP port: %d\n", rootDirectory, port)
+
+	return server
+}
+
+func serve(server *http.Server) {
+	if err := server.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
+		log.Fatalf("listen failed: %v\n", err)
+	}
+	log.Println("server stopped")
+}
+
+// Wait for interrupt signal to gracefully shutdown the server with
+func waitForSignal() {
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, os.Interrupt, syscall.SIGTERM)
+	<-signals
+}
+
+func shutdown(server *http.Server) {
+	ctx, stop := context.WithTimeout(context.Background(), time.Second)
+	defer stop()
+
+	log.Println("shutting down server")
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatalf("shutdown failed: %v\n", err)
+	}
 }
